a62547b18f208d51af942eb9cabff10dd2a47e99
Ed Heltzel <ed@rainyday.media>
2022-07-11 13:57:51 -0400

    adds 11ty syntaxhighlight plugin

--- src/_flightdeck/plugins.js
+++ src/_flightdeck/plugins.js
@@ -1,4 +1,7 @@
 const embedEverything = require("eleventy-plugin-embed-everything");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = (config) => {
   config.addPlugin(embedEverything);
+  config.addPlugin(syntaxHighlight);
 };
