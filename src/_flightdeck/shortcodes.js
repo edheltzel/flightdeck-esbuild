48b81428f2e6623fb25fdb528b8effb76a8b3c9d
Ed Heltzel <ed@rainyday.media>
2022-07-22 12:31:25 -0400

    adds versions shortcode

--- src/_flightdeck/shortcodes.js
+++ src/_flightdeck/shortcodes.js
@@ -1,7 +1,8 @@
 const copyright = require("./shortcodes/copyright");
 const codepen = require("./shortcodes/codepen");
-
+const version = require("./shortcodes/version");
 module.exports = (config) => {
   config.addShortcode("copyright", copyright);
   config.addShortcode("codepen", codepen);
+  config.addShortcode("version", version);
 };
