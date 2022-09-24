6602518ae49503fb9b3d1f26ad1951bf1a2cfd31
Ed Heltzel <ed@rainyday.media>
2022-09-24 12:33:24 -0400

    keeping projects in sync

--- .flightdeck.js
+++ .flightdeck.js
@@ -1,33 +1,20 @@
 // Eleventy Configuration
 const addWorkflow = require("./.flightdeck/workflow");
 const addFilters = require("./.flightdeck/filters");
-const addShortcodes = require("./.flightdeck/shortcodes");
-const addComponents = require("./.flightdeck/components");
 const addTransforms = require("./.flightdeck/transforms");
+const addShortcodes = require("./.flightdeck/shortcodes");
 const addPlugins = require("./.flightdeck/plugins");
+const addComponents = require("./.flightdeck/components");
 const isProd = process.env.ENVIRONMENT === "prod";
 
 module.exports = (config) => {
   addWorkflow(config);
   addFilters(config);
-  addShortcodes(config);
-  addComponents(config);
   addTransforms(config);
+  addShortcodes(config);
   addPlugins(config);
+  addComponents(config);
 
-  // Watch Targets
-  config.addWatchTarget("./src/assets");
-
-  // Layout Aliases
-  config.addLayoutAlias("page", "layouts/page.njk");
-
-  // Passthrough Copy
-  config.addPassthroughCopy({ "./node_modules/alpinejs/dist/cdn.min.js": "assets/js/alpine.js" });
-  config.addPassthroughCopy("./src/assets/fonts");
-
-  if (!isProd) {
-    config.addPassthroughCopy("./src/assets/images"); //no image optimization during development
-  }
 
   return {
     dir: {
