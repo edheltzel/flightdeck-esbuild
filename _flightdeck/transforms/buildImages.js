a5ad04b8ee3acec8b7ba5f594327ec7fed0ba4a4
Ed Heltzel <ed@rainyday.media>
2022-09-25 18:00:21 -0400

    flightdeck update - updates readme to include file structure - update `.flightdeck.js` to `.manifest.js` - renames `.flightdeck/` to `_flightdeck`

--- _flightdeck/transforms/buildImages.js
+++ _flightdeck/transforms/buildImages.js
@@ -2,7 +2,7 @@
 module.exports = (config) => {
   config.on("eleventy.after", () => {
     const spawn = require("child_process").spawn;
-    const child = spawn("optimizt", ["src/assets/images", "--output dist/"], {
+    const child = spawn("optimizt", ["--webp", "src/assets/images", "--output dist/"], {
       stdio: "inherit",
       shell: true,
     });
