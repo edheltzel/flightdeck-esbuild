bffeb3c08c1d25884214f035fd7f12fc00c6726b
Ed Heltzel <ed@rainyday.media>
2023-08-05 14:30:34 -0400

    updates workflow.js - adds `seo` dir - includes redirects and robots.txt

--- _flightdeck/workflow.js
+++ _flightdeck/workflow.js
@@ -2,24 +2,16 @@ const fs = require("fs");
 
 module.exports = (config) => {
   config.setQuietMode(true); // reduce console
-  /**
-   * @todo remove once 2.0 is released
-   */
-  config.setBrowserSyncConfig({
-    open: false,
-    notify: false,
-    // 404
-    callbacks: {
-      ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("dist/404/index.html");
+  config.setServerOptions({
+    /**
+     * @link https://github.com/11ty/eleventy/issues/1305
+     * @see https://www.11ty.dev/docs/dev-server/
+     * @description local dev server runs on port 8080 by default-> http://localhost:8080
+    */
+    showVersion: true, // show the server version number on the command line
+    showAllHosts: true, // show local network IP address for device testing
+    injectedScriptsFolder: ".flightdeck",
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
   });
 
   // Watch Targets
