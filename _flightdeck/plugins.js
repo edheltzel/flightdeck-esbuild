ec60198b05c0fd359258e0b6fda42771a5926174
Ed Heltzel <ed@rainyday.media>
2023-08-05 15:16:44 -0400

    button link component adds isProd

--- _flightdeck/plugins.js
+++ _flightdeck/plugins.js
@@ -1,3 +1,4 @@
+const isProd = process.env.ENV === "production";
 const embedEverything = require("eleventy-plugin-embed-everything");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
