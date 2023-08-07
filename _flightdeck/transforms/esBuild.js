794ac61c1e6898822884c635af718b4be6d60390
Ed Heltzel <ed@rainyday.media>
2023-08-07 16:23:42 -0400

    esbuild update

--- _flightdeck/transforms/esBuild.js
+++ _flightdeck/transforms/esBuild.js
@@ -1,4 +1,10 @@
-// esbuild & sass + autoprefixer
+/**
+ * Exports a function that runs BEFORE eleventy
+ * @param {Object} config - eleventy config object
+ * @summary This executes the esbuild tool on the output before build.
+ * @link https://esbuild.github.io/api/#build-api
+ * @link https://github.com/glromeo/esbuild-sass-plugin
+ */
 const isProd = process.env.ENV === "production";
 const esbuild = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
@@ -9,33 +15,32 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = (config) => {
   const flightdeck = console.log('\x1b[33m%s\x1b[0m', '[Flightdeck] ' + '>> esbuild complete');
   config.on("eleventy.before", async () => {
-      await esbuild.build({
-        bundle: true,
-        entryPoints: {
-          "assets/js/app": "./src/assets/js/app.js",
-          "assets/styles/style": "./src/assets/styles/style.scss",
-        },
-        loader: { ".scss": "css" },
-        minify: isProd,
-        outdir: "./dist",
-        sourcemap: !isProd,
-        plugins: [
-          sassPlugin({
-            async transform(source, resolveDir) {
-              let plugins = [];
-              if (isProd) {
-                plugins = [
-                  postcss([cssDeclarationSorter({ order: "smacss" })]),
-                  postcssPresetEnv({ stage: 0 }),
-                ];
-              }
-              const { css } = await postcss(plugins).process(source, { from: undefined });
-              return css;
-            },
-          }),
-        ]
-      });
-      // just some simple feedback to know esbuild and scss are running in yellow
-      return flightdeck;
+    await esbuild.build({
+      bundle: true,
+      entryPoints: {
+        "assets/js/app": "./src/assets/js/app.js",
+        "assets/styles/style": "./src/assets/styles/style.scss",
+      },
+      loader: { ".scss": "css" },
+      minify: isProd,
+      outdir: "./dist",
+      sourcemap: !isProd,
+      plugins: [
+        sassPlugin({
+          async transform(source, resolveDir) {
+            let plugins = [];
+            if (isProd) {
+              plugins = [
+                postcss([cssDeclarationSorter({ order: "smacss" })]),
+                postcssPresetEnv({ stage: 0 }),
+              ];
+            }
+            const { css } = await postcss(plugins).process(source, { from: undefined });
+            return css;
+          },
+        }),
+      ]
+    });
+    return flightdeck;
   });
 };
