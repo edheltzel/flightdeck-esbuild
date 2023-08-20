7dd08a1aa7bb18efa5cd6e7b29b81b98b3c7f13a
Ed Heltzel <ed@rainyday.media>
2023-08-20 09:46:14 -0400

    flightdeck console output - adds esbuild build time in seconds and milliseconds in the console

--- _flightdeck/transforms/esBuild.js
+++ _flightdeck/transforms/esBuild.js
@@ -5,13 +5,14 @@
  * @link https://esbuild.github.io/api/#build-api
  * @link https://github.com/glromeo/esbuild-sass-plugin
  */
-const { hrtime } = require('process');
+const chalk = require('kleur');
+const flightdeck = require('../../package.json');
 const isProd = process.env.ENV === "production";
 const esbuild = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 module.exports = (config) => {
   config.on("eleventy.after", async () => {
-    console.time('esbuild');
+    const startTime = Date.now();
     await esbuild.build({
       bundle: true,
       entryPoints: {
@@ -26,9 +27,7 @@ module.exports = (config) => {
         sassPlugin(),
       ]
     });
-    process.stdout.write('\x1b[33m[Flightdeck] >> completed  \x1b[0m');
-    console.timeEnd('esbuild');
-    const timeInfo = console.timeLog('esbuild');
-    process.stdout.write(` (${timeInfo} )\n`);
+    const endTime = (Date.now() - startTime) / 1000;
+    console.log(chalk.yellow(`[Flightdeck] >> esbuild finished in ${endTime} seconds (${endTime * 1000}ms, v${flightdeck.version})`));
   });
 };
