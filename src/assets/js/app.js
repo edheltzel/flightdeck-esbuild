38d62b42dd8f9c97a4feab5c5b9f3a5c4d913640
Ed Heltzel <ed@rainyday.media>
2023-08-23 17:13:40 -0400

    Rome format/lint

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,5 @@
 // test
-(() => { })();
+;(() => {})()
 //# sourceMappingURL=app.js.map
-const test = 'test';
-console.log('That\'s one small step for man, and one giant leap for mankind with a ' + test);
+const test = "test"
+console.log("That's one small step for man, and one giant leap for mankind with a " + test)
