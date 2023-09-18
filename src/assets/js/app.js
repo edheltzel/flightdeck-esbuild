2d1c56d3106ff52d46517bec0d3953624033eb40
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-09-18 11:19:09 -0400

    fixes #37 - themeSwitcher update - adds themeswither to app.js - includes local storage to keep the state of the theme selection - includes new svgs inside of navigation - update styles

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,27 @@
 (() => {
   // theme swap
-  const body = document.body;
+  const html = document.documentElement;
+  // Initialize theme
+  const initTheme = (theme) => {
+    if (theme === "dark") {
+      html.setAttribute("data-theme", "dark");
+    } else {
+      html.setAttribute("data-theme", "light");
+    }
+  };
+
+  const theme = localStorage.getItem("theme");
+  initTheme(theme);
+
   const eclipse = document.getElementById("mode");
 
   const toggleTheme = (state) => {
     if (state === "dark") {
       localStorage.setItem("theme", "light");
-      body.removeAttribute("data-theme");
+      html.setAttribute("data-theme", "light");
     } else if (state === "light") {
       localStorage.setItem("theme", "dark");
-      body.setAttribute("data-theme", "dark");
+      html.setAttribute("data-theme", "dark");
     } else {
       initTheme(state);
     }
