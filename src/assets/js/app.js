6848084fca5248fe40d288b130d5826e1cc2ed47
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-09-18 08:59:58 -0400

    setting up theme switcher

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,21 @@
-(() => {})();
-//# sourceMappingURL=app.js.map
-const test = "test ⚡";
-console.log(`That's one small step for man, and one giant leap for mankind with a ${test}`);
+(() => {
+  // theme swap
+  const body = document.body;
+  const eclipse = document.getElementById("mode");
+
+  const toggleTheme = (state) => {
+    if (state === "dark") {
+      localStorage.setItem("theme", "light");
+      body.removeAttribute("data-theme");
+    } else if (state === "light") {
+      localStorage.setItem("theme", "dark");
+      body.setAttribute("data-theme", "dark");
+    } else {
+      initTheme(state);
+    }
+  };
+
+  eclipse.addEventListener("click", () => {
+    toggleTheme(localStorage.getItem("theme"));
+  });
+})();
