0f0a97aa8f90db3f74d5ee1a2bd2c9a5cdc7db01
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-10 07:14:01 -0500

    Update Alpine.js version and add @alpinejs/persist dependency

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,9 +1,4 @@
 /**
- * Import Alpine.js powered theme toggle module and initialize it
- */
-// import "./modules/alpineModules/themeToggle.js";
-
-/**
  * Import Visual Guidance System cursor module and initialize it
  */
 import { VGSCursor } from "./modules/visualGuidanceSystem/vgsCursor.js";
