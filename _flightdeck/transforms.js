0e3f91fc75f41ce47a5cd79646e2c658a2329322
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-27 12:29:17 -0500

    Refactor transforms.js to improve build time and adds comments

--- _flightdeck/transforms.js
+++ _flightdeck/transforms.js
@@ -1,13 +1,21 @@
 /**
- * Transforms
- * @param {Object} config - eleventy config object
- * @description jamPack will increase build time dramatically.
+ * This module configures and exports a function that sets up various transformations for a given configuration.
  *
+ * @module transforms
+ *
+ * @requires module:./transforms/markdownIt - Markdown-it plugins for transforming markdown files.
+ * @requires module:./transforms/esBuild - ESBuild for compiling JavaScript and bundling assets.
+ * @requires module:./transforms/scss - SCSS compiler for transforming SCSS files into CSS.
+ * @requires module:./transforms/images - Image optimization tools for optimizing image assets.
+ *
+ * @param {Object} config - The configuration object to which transformations will be added.
+ *
+ * @returns {void} - The function does not have a return value; it modifies the passed configuration object.
  */
+
 const isProd = process.env.ENV === "production";
-// const jamPack = require("./transforms/jamPack"); // PROD only image and html optimization
 const { markdownIt } = require("./transforms/markdownIt"); // markdown-it plugins
-const esBuild = require("./transforms/esBuild"); // scss compiling & js bundling
+const esBuild = require("./transforms/esBuild"); // js bundling
 const scss = require("./transforms/scss"); // scss compiling
 const images = require("./transforms/images"); // image optimization
 module.exports = (config) => {
@@ -15,7 +23,4 @@ module.exports = (config) => {
   config.addPlugin(esBuild);
   config.addPlugin(scss);
   config.addPlugin(images);
-  // if (isProd) {
-  // config.addPlugin(jamPack);
-  // }
 };
