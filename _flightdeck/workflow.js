341bcbf7ed624f68be0a950a0998335e7522daad
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-29 11:53:53 -0500

    Update Eleventy dev server configuration

--- _flightdeck/workflow.js
+++ _flightdeck/workflow.js
@@ -2,15 +2,11 @@
  * Workflow Configuration
  * @summary keeps the main eleventy config file clean and is used to configure the local dev server and build directory.
  *
- * @file
- * This module exports a function that configures an Eleventy project.
- *
- * @module workflow
- *
- * @requires fs
- *
  * @param {Object} config - The Eleventy config object to be configured.
- *
+ * @property {Object} setServerOptions - Configure the local dev server.
+ * @link https://github.com/11ty/eleventy/issues/1305
+ * @see https://www.11ty.dev/docs/dev-server/
+ * @description local dev server runs on port 8080 by default-> http://localhost:8080
  */
 
 const fs = require("fs");
@@ -18,11 +14,6 @@ const fs = require("fs");
 module.exports = (config) => {
   config.setQuietMode(true); // reduce console
   config.setServerOptions({
-    /**
-     * @link https://github.com/11ty/eleventy/issues/1305
-     * @see https://www.11ty.dev/docs/dev-server/
-     * @description local dev server runs on port 8080 by default-> http://localhost:8080
-     */
     port: 4000, // old habits die hard - 4000 is the default port for Jekyll
     showVersion: true, // show the server version number on the command line
     showAllHosts: false, // show local network IP address for device testing
