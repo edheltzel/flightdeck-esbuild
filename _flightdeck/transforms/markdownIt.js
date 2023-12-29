addca659059cf781147a922b4c0e728f349ccc70
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-29 14:45:16 -0500

    Update markdown-it configuration

--- _flightdeck/transforms/markdownIt.js
+++ _flightdeck/transforms/markdownIt.js
@@ -1,13 +1,15 @@
 /**
  * This module configures and exports an instance of markdown-it with additional attributes support.
  * @module markdownIt
- * @see {@link https://github.com/markdown-it/markdown-it|markdown-it}
- * @see {@link https://github.com/arve0/markdown-it-attrs|markdown-it-attrs}
+ * @see {@link https://github.com/markdown-it/markdown-it}
+ * @see {@link https://github.com/arve0/markdown-it-attrs}
+ *
  * Configuration options for markdown-it.
- * @type {Object}
+ * @type {Object} markdownItOptions
  * @property {boolean} html - Enable HTML tags in source. Default is true.
  * @property {boolean} breaks - Convert '\n' in paragraphs into <br>. Default is true.
  * @property {boolean} linkify - Autoconvert URL-like text to links. Default is true.
+ * @see {@link https://markdown-it.github.io/markdown-it/#MarkdownIt.new} for more options.
  */
 
 const mdIt = require("markdown-it");
