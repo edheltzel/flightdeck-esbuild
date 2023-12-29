8b8ce161e1a07fab12809b1e2a77f0a256c13f1e
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-29 10:48:38 -0500

    updating jsdoc for better explanation.

--- _flightdeck/transforms/scss.js
+++ _flightdeck/transforms/scss.js
@@ -1,10 +1,21 @@
 /**
- * Adds Scss support to eleventy
- * @param {Object} config - eleventy config object
+ * This module adds Scss support to eleventy using the eleventy-sass plugin.
+ * It also uses PostCSS and Autoprefixer for processing CSS.
+ * @module scss
+ * @see {@link https://github.com/kentaroi/eleventy-sass/blob/main/docs/sass-options.md}
+ *
  * @summary uses eleventy-sass plugin to process scss files
- * @see https://github.com/kentaroi/eleventy-sass/blob/main/docs/sass-options.md
- * @property loadPaths: ["node_modules/bootstrap/scss"],
+ *
+ * @param {Object} config - eleventy config object
+ * @property {Object} sass - Sass options
+ * @property {Array} postcss - add PostCSS plugins ie: AutoPrefixer
+ * @property {Object} when - Conditional options
+ *
+ * @property {Array} loadPaths - Array of paths to include in the sass compilation.
+ * @description You can load Bootstrap's Scss file with @import "bootstrap"; by adding the following:
+ * @example loadPaths: ["node_modules/bootstrap/scss"],
  */
+
 const scss = require("eleventy-sass");
 const postcss = require("postcss");
 const autoprefixer = require("autoprefixer");
