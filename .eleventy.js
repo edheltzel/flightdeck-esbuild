40b6e724f3b0ac0bffd8a3696669f6ab15dd8ae4
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2023-12-31 08:23:43 -0500

    update .eleventy.js comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
-// Eleventy Configuration
+/**
+ * Eleventy Configuration
+ * @module .eleventy
+ */
+
 const addWorkflow = require("./_flightdeck/workflow");
 const addFilters = require("./_flightdeck/filters");
 const addTransforms = require("./_flightdeck/transforms");
@@ -6,16 +10,39 @@ const addShortcodes = require("./_flightdeck/shortcodes");
 const addPlugins = require("./_flightdeck/plugins");
 const addComponents = require("./_flightdeck/components");
 
+/**
+ * Eleventy configuration function
+ * @param {Object} config - Eleventy's configuration object
+ * @returns {Object} - Returns Eleventy's configuration options
+ */
 module.exports = (config) => {
+  /**
+   * Custom options for Flightdeck
+   * @type {Object}
+   * @property {boolean} useImageTransform - Whether to use image transforms or not
+   * @todo add more options
+   */
   const options = {
     useImageTransform: true,
   };
-  addWorkflow(config, options); // dev server, layout aliases, watch, passthrough copy
-  addTransforms(config, options); // esbuild, scss, image optimization
-  addPlugins(config); // eleventy plugins
-  addShortcodes(config); // copyright year, youtube embeds, etc.
-  addFilters(config); // universal filters
-  addComponents(config); // custom components for Flightdeck Airframe
+
+  // Configure 11ty development server, layout aliases, watch, passthrough copy
+  addWorkflow(config, options);
+
+  // Custom plugins that integrate esbuild, scss, image optimization
+  addTransforms(config, options);
+
+  // Add eleventy plugins
+  addPlugins(config);
+
+  // Custom shortcodes for Nunjucks/Liquid template copyright year, youtube embeds, etc.
+  addShortcodes(config);
+
+  // Custom universal filters for Nunjucks/Liquid templates
+  addFilters(config);
+
+  // Custom components for Flightdeck Autopilot UI
+  addComponents(config);
 
   // 11ty configuration options
   return {
