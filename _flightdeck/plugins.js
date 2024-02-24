dd4b6f22e72723ebb34951899a5384ccd6fec0f6
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-02-24 11:32:36 -0500

    adding eleventy-navigation plugin

--- _flightdeck/plugins.js
+++ _flightdeck/plugins.js
@@ -13,8 +13,10 @@
 
 const embedEverything = require("eleventy-plugin-embed-everything");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const navigation = require("@11ty/eleventy-navigation");
 
 module.exports = (config) => {
   config.addPlugin(embedEverything);
   config.addPlugin(syntaxHighlight);
+  config.addPlugin(navigation);
 };
