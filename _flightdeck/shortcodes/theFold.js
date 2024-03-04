1613c946c25bce8912540e6e3a6d8588e4f1d79e
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-03-04 14:08:00 -0500

    removing thefold

--- _flightdeck/shortcodes/theFold.js
+++
@@ -1,9 +0,0 @@
-/**
- * Jampack Optimize above the fold
- * @description For details, see {@link}
- * @link https://jampack.divriots.com/features/optimize-above-the-fold/
- * @usage {% thefold %}
- */
-module.exports = (theFold) => {
-  return "<the-fold></the-fold>";
-};
