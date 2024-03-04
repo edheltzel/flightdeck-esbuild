690e53058679e6fa3c3294e029db20f08145f5c3
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-03-04 14:41:56 -0500

    Remove VGSCursor import and invocation, and update modal comment

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,9 @@
 /**
  * @file This is the main entry point for the application. It imports and initializes Alpine.js and VGSCursor.
  * @requires module:alpinejs
- * @requires module:./modules/visualGuidanceSystem/vgs-cursor.js
  */
 
 import Alpine from "alpinejs";
-import { VGSCursor } from "./modules/visualGuidanceSystem/vgs-cursor.js";
 
 // Assigning Alpine to the global window object
 window.Alpine = Alpine;
@@ -15,9 +13,3 @@ window.Alpine = Alpine;
  * @see {@link https://github.com/alpinejs/alpine#start|Alpine.start}
  */
 Alpine.start();
-
-/**
- * Invokes VGSCursor function
- * @see {@link ./modules/visualGuidanceSystem/vgsCursor.js|VGSCursor}
- */
-// document.addEventListener('DOMContentLoaded', VGSCursor);
