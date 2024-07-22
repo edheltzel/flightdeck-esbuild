90728c501ae50840c72fbcce4bb000e65be99d13
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-07-22 09:12:47 -0400

    flightdeck update
    renames esbuild2 and includes the legacy esbuild processing that uses the eleventy.after - keeping this file for the time being even thought we are not using it.

---
+++ src/_flightdeck/transforms/_legacyEsBuild.js
@@ -0,0 +1,28 @@
+/**
+ * ESBuild Transform function
+ * @callback transformJs
+ * @param {Object} config - eleventy config object
+ * @async eleventy.after hook to run esbuild
+ * @summary This executes the esbuild on the output after the eleventy build.
+ * @link https://esbuild.github.io/api/#build-api
+ */
+const isProd = process.env.ENV === "production";
+const esbuild = require("esbuild");
+
+const transformJs = (config) => {
+  config.on("eleventy.after", async () => {
+    await esbuild.build({
+      entryPoints: { "assets/js/app": "./src/assets/js/app.js" },
+      bundle: true,
+      outdir: "dist",
+      minify: isProd,
+      sourcemap: !isProd,
+      splitting: true,
+      format: "esm",
+      logLevel: "warning",
+      metafile: true,
+    });
+  });
+};
+
+module.exports = { transformJs };
