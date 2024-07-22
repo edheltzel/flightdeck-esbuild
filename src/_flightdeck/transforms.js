7f6758218c8d5ed262e32dbafaafb80741deb5dc
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-07-22 09:17:40 -0400

    chore: Update transforms.js to optimize image processing
    removes transformsImageDir to transformImages to fix a ts-check error

--- src/_flightdeck/transforms.js
+++ src/_flightdeck/transforms.js
@@ -1,14 +1,11 @@
+// @ts-check
+
 /**
  * Transforms
  * @summary This adds custom transforms to Eleventy, keeping Eleventy in control of the entire development and build processes.
  *
- * @file
- * This module exports a function that adds several transformations to an Eleventy config object.
- *
- * @module transforms
- *
- * @param {Object} config - The Eleventy config object to which the transformations will be added.
- * @param {boolean} useImageDirTransform - Whether or not to use image transforms (default: true).
+ * @param {import("@11ty/eleventy").UserConfig} config - The Eleventy config object to which the transformations will be added.
+ * @param {{useImageDirTransform: boolean}} options - Custom options for configuring transforms.
  *
  * @todo drop scss for css + lightningCSS
  * @todo Add Scss configuration in README for those that still want to use Scss.
@@ -16,20 +13,18 @@
 
 const isProd = process.env.ENV === "production";
 const { markdownIt } = require("./transforms/markdownIt"); // markdown-it plugins
-const { transformImageDir } = require("./transforms/allimages"); // optimize all images in src/assets/images
+const { transformImages } = require("./transforms/allimages"); // optimize all images in src/assets/images
 const minifyHtml = require("./transforms/minifyHtml");
 const { transformJs } = require("./transforms/esBuild"); // js bundling
 const lightningCss = require("./transforms/lightning"); // css bundling
-// const { transformScss } = require("./transforms/scss"); // scss compiling
 
 module.exports = (config, options) => {
-  // config.addPlugin(transformScss);
   config.setLibrary("md", markdownIt);
   config.addPlugin(transformJs);
   config.addPlugin(lightningCss);
 
   if (options.useImageDirTransform) {
-    config.addPlugin(transformImageDir);
+    config.addPlugin(transformImages);
   }
   // production build only
   if (isProd) {
