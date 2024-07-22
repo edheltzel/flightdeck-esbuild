24b8f0a95d9167b7f90cf7a4834a48bfc29c5c8e
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-07-22 13:23:29 -0400

    feature: adds support for markdown-it-bracketed-spans
    - this adds the ability to include classes/ids to bracketed items in markdown... very helpful for styling and using tailwind.

--- src/_flightdeck/transforms/markdownIt.js
+++ src/_flightdeck/transforms/markdownIt.js
@@ -5,16 +5,22 @@
  * @module markdownIt
  * @requires markdown-it
  * @requires markdown-it-attrs
+ * @requires markdown-it-bra
+ *
+ * @see {@link https://github.com/markdown-it/markdown-it}
+ * @see {@link https://github.com/arve0/markdown-it-attrs}
+ * @see {@link https://github.com/mb21/markdown-it-bracketed-spans}
  */
 
 const mdIt = require("markdown-it");
 const mdItAttrs = require("markdown-it-attrs");
-
+const mdItBracketedSpans = require("markdown-it-bracketed-spans");
 /**
  * @typedef {Object} MarkdownItOptions
  * @property {boolean} html - Enable HTML tags in source
  * @property {boolean} breaks - Convert '\n' in paragraphs into <br>
  * @property {boolean} linkify - Autoconvert URL-like text to links
+ * @see {@link https://markdown-it.github.io/markdown-it/#MarkdownIt.new} for more options.
  */
 
 /** @type {MarkdownItOptions} */
@@ -24,6 +30,6 @@ const markdownItOptions = {
   linkify: true,
 };
 
-const markdownIt = mdIt(markdownItOptions).use(mdItAttrs);
+const markdownIt = mdIt(markdownItOptions).use(mdItAttrs).use(mdItBracketedSpans);
 
 exports.markdownIt = markdownIt;
