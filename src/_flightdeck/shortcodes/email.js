9f5e49f90c4b59890a8cb8a81360feff764d68ca
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-08-02 14:37:43 -0400

    feature: email shortcode
    - adds email shortcode with obfuscation

---
+++ src/_flightdeck/shortcodes/email.js
@@ -0,0 +1,20 @@
+/**
+ * Email obfuscation shortcode with CSS
+ * @param {Object} [params={}] - The parameters for email obfuscation
+ * @param {string} [params.address] - The email address to obfuscate
+ * @param {string} [params.honeypot='honeypot'] - The honeypot text to insert (will be hidden with CSS)
+ * @returns {string} HTML string with obfuscated email
+ * @example
+ * // Basic usage
+ * {% email address="your-email@example.com" %}
+ * // With custom honeypot
+ * {% email address="your-email@example.com", honeypot="nospam" %}
+ * @typedef {Object} EmailParams
+ * @property {string} [address] - The email address to obfuscate
+ * @property {string} [honeypot] - The honeypot text to insert
+ * @type {(params?: EmailParams) => string}
+ */
+module.exports = (params = {}) => {
+  const { address, honeypot = 'honeypot' } = params;
+  return `<span class="email">${address}<b>${honeypot}.com</b></span>`;
+};
