07c04922e5b58c2dae8002a2476809f8ced35068
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-09-02 06:55:40 -0400

    trying to resolve tw compiling cascade with the JIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-		"./src/**/*.{html,js,njk,md,liquid,webc,svg}"
-	],
-	darkMode: 'class',
-  theme: {
-		container:{
+	content: ["./src/**/*.{html,js,njk,md,liquid,webc,svg}"],
+	darkMode: "class",
+	theme: {
+		container: {
 			center: true,
 		},
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        'inter-tight': ['Inter Tight', 'sans-serif']
-      },
-			colors:{
+		extend: {
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				"inter-tight": ["Inter Tight", "sans-serif"],
+			},
+			colors: {
 				primary: "var(--color-primary)",
 				alpha: "var(--color-alpha)",
 				peach: "var(--color-peach)",
 				charcoal: "var(--color-charcoal)",
 				text: "var(--color-text)",
-			}
+			},
 		},
-  },
-  plugins: [],
-}
+	},
+	plugins: [],
+};
