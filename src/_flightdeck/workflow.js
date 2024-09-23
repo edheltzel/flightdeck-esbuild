c813a02633bfe44158dbd7e9ed243928db912599
Ed Heltzel <402910+edheltzel@users.noreply.github.com>
2024-09-23 15:25:11 -0400

    refactor workflow.js for consistent indentation and formatting adjustments

--- src/_flightdeck/workflow.js
+++ src/_flightdeck/workflow.js
@@ -9,29 +9,29 @@
  * @see https://www.11ty.dev/docs/dev-server/
  */
 module.exports = (config, options) => {
-  config.setQuietMode(true); // reduce console
-  config.setServerOptions({
-    port: 54321, // like Astro
-    showVersion: true, // show the server version number on the command line
-    showAllHosts: false, // show local network IP address for device testing
-  });
+	config.setQuietMode(true); // reduce console
+	config.setServerOptions({
+		port: 54321, // like Astro
+		showVersion: true, // show the server version number on the command line
+		showAllHosts: false, // show local network IP address for device testing
+	});
 
-  /* --- Watch Targets --- */
-  config.addWatchTarget("./src/assets");
+	/* --- Watch Targets --- */
+	config.addWatchTarget("./src/assets");
 
-  /* --- Passthrough Copy --- */
-  config.addPassthroughCopy({
-    "./src/_static": "./", // root level files ie: _redirects, robots.txt, favicon.io,etc
-    "./src/assets/fonts": "./assets/fonts"
-  });
+	/* --- Passthrough Copy --- */
+	config.addPassthroughCopy({
+		"./src/_static": "./", // root level files ie: _redirects, robots.txt, favicon.io,etc
+		"./src/assets/fonts": "./assets/fonts",
+	});
 
-  if (!options.useImageDirTransform) {
-    config.addPassthroughCopy("./src/assets/images"); // copy images if useImageDirTransform is false
-  }
+	if (!options.useImageDirTransform) {
+		config.addPassthroughCopy("./src/assets/images"); // copy images if useImageDirTransform is false
+	}
 
-  /* --- Layout Aliases --- */
-  config.addLayoutAlias("base", "layouts/base.njk");
-  config.addLayoutAlias("default", "layouts/base.njk");
-  config.addLayoutAlias("page", "layouts/page.njk");
-  config.addLayoutAlias("post", "layouts/post.njk");
+	/* --- Layout Aliases --- */
+	config.addLayoutAlias("base", "layouts/base.njk");
+	config.addLayoutAlias("default", "layouts/base.njk");
+	config.addLayoutAlias("page", "layouts/page.njk");
+	config.addLayoutAlias("post", "layouts/post.njk");
 };
